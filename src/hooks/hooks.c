a4e964db6a1d854a1833399126edf2bcc764ba4e
nponchon <nponchon@student.42.fr>
2025-02-04 17:52:15 +0100

    added some reach to open/close doors

--- src/hooks/hooks.c
+++ src/hooks/hooks.c
@@ -6,7 +6,7 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:05:26 by nponchon          #+#    #+#             */
-/*   Updated: 2025/02/04 17:44:06 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:51:04 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@
 	Opens and closes doors. This needs to be time-based instead of frames,
 	so that there is a cooldown between opening and closing.
 	TODO
-	Implement sprite animation when opening and closing.
+	Special texture for door, implement sprite animation.
 */
 void	cub_open_close_doors(t_cub *cub)
 {
 	if (cub->map[(int)(cub->player->pos.x + cub->player->dir.x \
-		* cub->player->speed)][(int)cub->player->pos.y] == 2)
+		* cub->player->speed * 7)][(int)cub->player->pos.y] == 2)
 		cub->map[(int)(cub->player->pos.x + cub->player->dir.x \
-		* cub->player->speed)][(int)cub->player->pos.y] = -1;
+		* cub->player->speed * 7)][(int)cub->player->pos.y] = -1;
 	else if (cub->map[(int)(cub->player->pos.x + cub->player->dir.x \
-		* cub->player->speed)][(int)cub->player->pos.y] == -1)
+		* cub->player->speed * 7)][(int)cub->player->pos.y] == -1)
 		cub->map[(int)(cub->player->pos.x + cub->player->dir.x \
-		* cub->player->speed)][(int)cub->player->pos.y] = 2;
+		* cub->player->speed * 7)][(int)cub->player->pos.y] = 2;
 	else if (cub->map[(int)cub->player->pos.x][(int)(cub->player->pos.y \
-		+ cub->player->dir.y * cub->player->speed)] == 2)
+		+ cub->player->dir.y * cub->player->speed * 7)] == 2)
 		(cub->map[(int)cub->player->pos.x][(int)(cub->player->pos.y \
-		+ cub->player->dir.y * cub->player->speed)] = -1);
+		+ cub->player->dir.y * cub->player->speed * 7)] = -1);
 	else if (cub->map[(int)cub->player->pos.x][(int)(cub->player->pos.y \
-		+ cub->player->dir.y * cub->player->speed)] == -1)
+		+ cub->player->dir.y * cub->player->speed * 7)] == -1)
 		(cub->map[(int)cub->player->pos.x][(int)(cub->player->pos.y \
-		+ cub->player->dir.y * cub->player->speed)] = 2);
+		+ cub->player->dir.y * cub->player->speed * 7)] = 2);
 }
 
 /*
